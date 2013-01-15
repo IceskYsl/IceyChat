6cd63c851bb7bebd5634fb238b413004eedc37a7
Iceskysl <iceskysl@gmail.com>
2013-01-15 23:17:06 +0800

    init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IceyChat::Application.config.secret_token = '03aad32834a54ef15cbd19c49cabe42bacfc0754f1e44b285a8262ab74694e7c358e73f2106e3f9b8d7435d0ea168245858d8630400b2e14af2e6f3db31cb08e'
